af940e6c50ddfa11b637f50730c2a1fa1b4b4431
Mikołaj Krajewski <mikolajkrajewski.gra@gmail.com>
2020-11-25 13:21:20 +0100

    Added cube editor script and created simple chess board with pieces

---
+++ Assets/ChessBlockEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class ChessBlockEditor : MonoBehaviour
+{
+    float gridSize = 3.25f;
+    void Update()
+    {
+        SnapToGrid();
+        UpdateName();
+    }
+
+    private void SnapToGrid()
+    {
+        transform.position = new Vector3(GetGridPosition().x * gridSize, 0f, GetGridPosition().y * gridSize);
+    }
+
+    private void UpdateName()
+    {
+        gameObject.name = GetGridPosition().x + " , " + GetGridPosition().y;
+    }
+    Vector2Int GetGridPosition()
+    {
+        return new Vector2Int(Mathf.RoundToInt(transform.position.x / gridSize), Mathf.RoundToInt(transform.position.z / gridSize));
+    }
+}
